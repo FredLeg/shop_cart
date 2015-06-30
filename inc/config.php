4c8e9b6497bcbbaa9249048fde41a214bbd38008
- <->
2015-06-30 17:08:22 +0200

    utilise user.php et ['cart']

--- inc/config.php
+++ inc/config.php
@@ -1,7 +1,9 @@
 <?php
 require_once 'inc/db.php';
 require_once 'inc/func.php';
+require_once 'inc/user.php';
 
 session_name('shop_session');
 session_start();
 
+if (!isset($_SESSION['cart'])) $_SESSION['cart'] = [];
